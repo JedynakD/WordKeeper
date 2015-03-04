ac0b46c3b2c0cbe5f36f7992ed9cdf49fadb0bef
JedynakD <djedynak90@gmail.com>
2015-03-04 15:52:58 +0100

    added aggregation

--- src/main/java/org/jedynakd/WordsKeeper.java
+++ src/main/java/org/jedynakd/WordsKeeper.java
@@ -5,27 +5,27 @@ import java.util.List;
 
 public class WordsKeeper {
 
-	private List<String> words = new ArrayList<>();
+    private List<String> words = new ArrayList<>();
 
-	public List<String> getWords() {
-		return words;
-	}
+    public List<String> getWords() {
+        return words;
+    }
 
-	public WordsKeeper() {
-		words.add("monitor");
-		words.add("keyboard");
-		words.add("mouse");
-		words.add("microphone");
-		words.add("hardware");
-		words.add("software");
-		words.add("peripherals");
-		words.add("motherboard");
-		words.add("memory");
-		words.add("adapter");
-	}
+    public WordsKeeper() {
+        words.add("monitor");
+        words.add("keyboard");
+        words.add("mouse");
+        words.add("microphone");
+        words.add("hardware");
+        words.add("software");
+        words.add("peripherals");
+        words.add("motherboard");
+        words.add("memory");
+        words.add("adapter");
+    }
 
-	public String getOneWord(int whichWord) {
-		String oneWord = words.get(whichWord);
-		return oneWord;
-	}
+    public String getOneWord(int whichWord) {
+        String oneWord = words.get(whichWord);
+        return oneWord;
+    }
 }
